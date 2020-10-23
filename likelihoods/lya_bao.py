7a3105549fb3689ebed4ec6a066cc62d5eb5a4df
Andrei Cuceu <andrei.cuceu.14@ucl.ac.uk>
2020-10-23 12:58:23 +0100

    First version of a Ly-alpha BAO likelihood from a DR16 scan

---
+++ likelihoods/lya_bao.py
@@ -0,0 +1,73 @@
+from typing import Optional
+from cobaya.likelihoods._base_classes import _bao_prototype
+from cobaya.likelihood import Likelihood
+import numpy as np
+from scipy.interpolate import RectBivariateSpline
+import scipy.constants
+
+class lya_bao(Likelihood):
+
+    path: str
+    redshift: Optional[float]
+    DA_over_rs_fid: Optional[float]
+    c_over_Hz_rs_fid: Optional[float]
+
+    def initialize(self):
+        """
+         Prepare any computation, importing any necessary code, files, etc.
+        """
+        data = np.loadtxt(self.path)
+
+        at = np.array(sorted(set(data[:,0])))
+        ap = np.array(sorted(set(data[:,1])))
+
+        N_at = at.shape[0]
+        N_ap = ap.shape[0]
+        grid = np.zeros((N_at,N_ap))
+
+        for i in range(N_ap):
+            # Filter the data to only those corresponding to the ap value.
+            indices = (data[:,1] == ap[i])
+            scan_chunk = data[indices, :]
+
+            # Ensure that they're sorted by at value.
+            scan_chunk = scan_chunk[scan_chunk[:,0].argsort()]
+            
+            # Add the chi2 column to the grid.
+            # Note that the grid is of shape (N_at,N_ap)
+            grid[:,i] = scan_chunk[:,2]
+
+        #Make the interpolator (x refers to at, y refers to ap).
+        self.interpolator = RectBivariateSpline(at, ap, grid, kx=1, ky=1)
+
+    def get_requirements(self):
+        """
+         return dictionary specifying quantities calculated by a theory code are needed
+        """
+        reqs = {
+            "DA_over_rs": {
+                "angular_diameter_distance": {"z": self.redshift},
+                "rdrag": None},
+            "Hz_rs": {
+                "Hubble": {"z": self.redshift},
+                "rdrag": None},
+        }
+        
+        return reqs
+
+    def logp(self, **params_values):
+        """
+        Taking a dictionary of (sampled) nuisance parameter values params_values
+        and return a log-likelihood.
+
+        e.g. here we calculate chi^2  using cls['tt'], H0_theory, my_foreground_amp
+        """
+        rd = self.provider.get_param("rdrag")
+        da = self.provider.get_angular_diameter_distance(self.redshift)
+        hubble = self.provider.get_Hubble(self.redshift, units="km/s/Mpc")
+        c = scipy.constants / 1000
+        at = (da / rd) / self.DA_over_rs_fid
+        ap = (c / (hubble * rd)) / self.c_over_Hz_rs_fid
+
+        chi2 = float(self.interpolator(at, ap)[0])
+        return -chi2 / 2
